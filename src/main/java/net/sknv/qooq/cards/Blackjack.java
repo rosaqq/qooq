26e740d02f91fd9073070580f2955b1f91bc04f1
Tiago Rosa <secknv@gmail.com>
2020-05-30 16:08:33 +0100

    removed return type from bet function

--- src/main/java/net/sknv/qooq/cards/Blackjack.java
+++ src/main/java/net/sknv/qooq/cards/Blackjack.java
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.entities.Message;
 
 public class Blackjack {
 
-    public static boolean bet(Message msg) {
+    public static void bet(Message msg) {
         String[] text = msg.getContentRaw().split(" ");
         if (text.length < 3) {
             msg.getChannel().sendMessage("Please bet some money").queue();
@@ -16,11 +16,10 @@ public class Blackjack {
             }
             catch (NumberFormatException a) {
                 msg.getChannel().sendMessage("That's not a number idiot").queue();
-                return false;
+                return;
             }
             msg.getChannel().sendMessage("You bet " + money + " moneys").queue();
         }
-        return true;
     }
 
 }
